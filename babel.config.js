8f58bcf348fb3de453fcc0fcf8805c6bc2005ff0
khoi1908vn <khoimle7@gmail.com>
2026-06-04 02:00:21 +0700

    fix:

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+      "babel-preset-expo",
+      "nativewind/babel"
     ],
     plugins: [
-      "nativewind/babel",
       "react-native-reanimated/plugin"
     ],
   };
